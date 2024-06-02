861f0319e05a0e0fb325105f28a82c0cfd38d5ee
vnkgd <vnkgd@mail.ru>
2024-06-02 13:45:56 +0200

    Vue

---
+++ interno-by-vue/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+        <div class="overlay"></div>
+<HeaderBlock />
+<MainBlock/>
+<FooterBlock/>
+<ArticlesSort/>
+<ArticlesFilter/>
+    </div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import HeaderBlock from './components/HeaderBlock.vue'
+import MainBlock from './components/MainBlock.vue'
+import FooterBlock from './components/FooterBlock.vue'
+import ArticlesSort from './pages/ArticlesSort.vue'
+import ArticlesFilter from './pages/ArticlesFilter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    HeaderBlock,
+    MainBlock,
+    FooterBlock,
+    ArticlesSort,
+    ArticlesFilter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ interno-by-vue/src/components/HeaderBlock.vue
@@ -0,0 +1,45 @@
+<template>
+        <header class="header">
+            <div class="header__container">
+                <a href="#" class="header__logo">
+                    <img src="../img/logo.svg" alt="" />
+                </a>
+                <nav class="header__menu menu">
+                    <ul class="menu__list">
+                        <li class="menu__item">
+                            <a class='menu__link' href='index.html'>Home</a>
+                        </li>
+                        <!-- <li class="menu__item">
+                            <a class='menu__link' href='/about'>About Us</a>
+                        </li> -->
+                        <!-- <li class="menu__item">
+                            <a class='menu__link' href='/services'>Services</a>
+                        </li> -->
+                        <li class="menu__item">
+                            <a class='menu__link' href='articlesFilter.html'>Articles Filter</a>
+                        </li>
+                        <li class="menu__item">
+                            <a class='menu__link' href='articlesSort.html'>Articles Sort</a>
+                        </li>
+                        <li class="menu__item">
+                            <a class='menu__link' href='projects.html'>Projects</a>
+                        </li>
+                        <li class="menu__item">
+                            <a class='menu__link' href='blog.html'>Blog</a>
+                        </li>
+                        <!-- <li class="menu__item">
+                            <a class='menu__link' href='/contact'>Contact</a>
+                        </li> -->
+                    </ul>
+                </nav>
+                <div class="header__actions">
+                    <a href="#" class="header__search">
+                        <img src="../img/icons/search.svg" alt="search" />
+                    </a>
+                    <div class="header__burger icon-menu">
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+        </header>
+  </template>
